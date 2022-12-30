f946c722cb317df82e65cafe479a00bcd480559c
Zzenith <zzenithecho@gmail.com>
2022-12-30 17:18:41 -0500

    Update superCoolCode.js

--- superCoolCode.js
+++ superCoolCode.js
@@ -17,7 +17,7 @@ const data = {
     "session_id": "F4896F46-EE50-4005-A9F1-7734E4BD8861",
     "team_idx": 0
 };
-fetch('192.168.43.60:6721/join_session', {
+fetch('192.168.43.200:6721/join_session', {
     mode: 'no-cors',
     method: 'POST', // or 'PUT'
     headers: {
