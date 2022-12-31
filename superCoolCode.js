175563666228dcf4dd5924bb735896ef087cf9b4
gitzzenith <zzenithecho@gmail.com>
2022-12-30 23:27:24 -0500

    changes

--- superCoolCode.js
+++ superCoolCode.js
@@ -6,7 +6,7 @@ async function postData(sessionid, ipFrag) {
     };
 
     //fetchUrl = 'http://127.0.0.' + ipFrag + ':6721/join_session'; // desktop
-    fetchUrl = 'http://192.168.43.' + ipFrag + ':6721/join_session'; // quest
+    fetchUrl = '192.168.43.' + ipFrag + ':6721/join_session'; // quest
 
     fetch(fetchUrl, {
         mode: 'no-cors',
@@ -16,6 +16,12 @@ async function postData(sessionid, ipFrag) {
         },
         body: JSON.stringify(data),
     })
+        /*.then(function(response) {
+            console.log(response.ok);
+        })
+        .catch(function(error) {
+            document.getElementById('errOut').value = "Fetch error " + error;
+        }*/
 }
 
 // Runs when join button is clicked
@@ -23,7 +29,15 @@ function handleSubmit(event) {
     event.preventDefault();
     var ipFrag = document.getElementById('ipFrag').value;
     
-    postData("F10C1B4F-382E-49D9-857E-1CADC7DC675C", ipFrag);
+    try {
+        postData("F10C1B4F-382E-49D9-857E-1CADC7DC675C", ipFrag);
+    }
+    catch(err)
+    {
+        console.log("error, " + err);
+    }
+    
+    
     console.log(ipFrag);
     return false;
 }
